b7531803a61ee03a053ba3abcda1f050f3f1728a
Demidov Evgeniy <demidov.e@citilink.ru>
2020-10-09 18:36:37 +0300

    init editor

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Text = () => (
-    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <textarea name="" id="" cols="30" rows="10"/>
 );
 
 function App() {
